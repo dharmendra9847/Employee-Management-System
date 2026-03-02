90f5856d9d6b97d8db947e60486529f5037648d5
Dharmendra Kumar <118713094+dharmendra9847@users.noreply.github.com>
2026-03-03 01:56:20 +0530

    EMS-MINI-PROJECT SECOND COMMIT

--- EMS-MINI-PROJECT/src/com/start/Launch.java
+++ EMS-MINI-PROJECT/src/com/start/Launch.java
@@ -1,5 +1,7 @@
 package com.start;
 
+import java.util.Scanner;
+
 public class Launch {
 
     void main(){
@@ -17,5 +19,48 @@ public class Launch {
         *
         * Hint: Scanner, sop, Switch
         */
-    }
+
+        //GREETING
+        IO.println("\n(:****** WELCOME TO EMPLOYEE MANAGEMENT SYSTEM ******:)");
+        IO.println("-------------------------------------------------------\n");
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true){
+            IO.println("Press-1 : Add Employee");
+            IO.println("Press-2 : Read All Employee");
+            IO.println("Press-3 : Delete Single Employee");
+            IO.println("Press-4 : Update Single Employee");
+            IO.println("Press-5 : Exit\n");
+
+            IO.print("Enter Your Choice: ");
+            int choice = sc.nextInt();
+
+            if (choice == 5){
+                IO.println("Thank You for using my service!");
+                break;
+            }
+
+            switch (choice){
+                case 1:
+                    IO.println("Add Employee\n");
+                    break;
+                case 2:
+                    IO.println("Read All Employee\n");
+                    break;
+                case 3:
+                    IO.println("Delete Single Employee\n");
+                    break;
+                case 4:
+                    IO.println("Update Single Employee\n");
+                    break;
+                case 5:
+                    IO.println("Exit");
+                    break;
+                default:
+                    IO.println("Invalid Choice\n");
+                    break;
+            }//end of switch
+        }//end of while
+    }//end of main
 }
