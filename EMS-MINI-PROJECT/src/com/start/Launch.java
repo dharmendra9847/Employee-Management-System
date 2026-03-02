4181847aa2839f1fa58e65a5c21e8a988d53de3e
Dharmendra Kumar <118713094+dharmendra9847@users.noreply.github.com>
2026-03-03 01:55:04 +0530

    EMS-MINI-PROJECT FIRST STEP

---
+++ EMS-MINI-PROJECT/src/com/start/Launch.java
@@ -0,0 +1,21 @@
+package com.start;
+
+public class Launch {
+
+    void main(){
+
+        //CREATE A MENU
+        /*
+        * ****** WELCOME TO EMPLOYEE MANAGEMENT SYSTEM ******
+        *
+        * Press-1 : Add Employee
+        * Press-2 : Read All Employee
+        * Press-3 : Delete Single Employee
+        * Press-4 : Update Single Employee
+        * Press-5 : Exit
+        * Enter Your Choice:
+        *
+        * Hint: Scanner, sop, Switch
+        */
+    }
+}
